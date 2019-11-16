9f094456c407f9473c91a06fc55b26f417a5ea11
Dschgame <dschg.medrei@gmail.com>
2019-11-16 16:59:52 +0100

    7

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,8 +11,8 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args){
-        //new Panel().start();
-        Connecter c = new Connecter("jdbc:mysql://mysql01.manitu.net","u38937", "V2ZDudBWdT69");
+        new Panel().start();
+        //Connecter c = new Connecter("jdbc:mysql://mysql01.manitu.net","u38937", "V2ZDudBWdT69");
         //System.out.println(c.read("Bombs", 2));
         //System.out.println("soos");
         //c.delete("Bombs");
