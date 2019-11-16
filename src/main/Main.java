661114762464c0fc811479cc56e744262e6179ac
Dschgame <dschg.medrei@gmail.com>
2019-11-16 16:56:40 +0100

    7

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,22 @@
 package main;
 
+import conn.Connecter;
 import gui.Panel;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        new Panel().start();
+    public static void main(String[] args){
+        //new Panel().start();
+        Connecter c = new Connecter("jdbc:mysql://mysql01.manitu.net","u38937", "V2ZDudBWdT69");
+        //System.out.println(c.read("Bombs", 2));
+        //System.out.println("soos");
+        //c.delete("Bombs");
+        //System.out.println("soos");
+        //c.write("Bombs", "Row, Column", "' 5 ' , '5' ");
     }
-
 }
